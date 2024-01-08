d8f7e8650abe34b6621d216c557ab2dc94e571f4
Josh Tynjala <joshtynjala@bowlerhat.dev>
2024-01-08 09:51:57 -0800

    script to generate sitemap

---
+++ generate-sitemap.mjs
@@ -0,0 +1,47 @@
+import * as fs from "node:fs";
+import * as path from "node:path";
+
+let rootDirName = process.argv[2];
+if (!rootDirName) {
+	console.error(`Root path not specified`);
+	process.exit(1);
+}
+
+let rootPath = rootDirName;
+if (!path.isAbsolute(rootPath)) {
+	rootPath = path.resolve(process.cwd(), rootPath);
+}
+
+if (!fs.existsSync(rootPath)) {
+	console.error(`Path does not exist: ` + rootPath);
+	process.exit(1);
+}
+
+let lastMod = new Date();
+
+let sitemap = `<?xml version="1.0" encoding="utf-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+`;
+
+function addFilesFromDirectory(dirPath, baseUrlPathname) {
+	for (const currentFileName of fs.readdirSync(dirPath)) {
+		const currentPath = path.resolve(dirPath, currentFileName)
+		if (fs.statSync(currentPath).isDirectory()) {
+			addFilesFromDirectory(currentPath, baseUrlPathname + "/" + currentFileName);
+			continue;
+		}
+		if (path.extname(currentFileName) === ".html") {
+			sitemap += `  <url>
+    <loc>https://api.feathersui.com/${baseUrlPathname}/${currentFileName}</loc>
+    <lastmod>${lastMod.toISOString()}</lastmod>
+  </url>
+`;
+		}
+	}
+}
+
+addFilesFromDirectory(rootPath, rootDirName);
+
+sitemap += `</urlset>`;
+
+fs.writeFileSync(`sitemap.xml`, sitemap, {encoding: "utf8"});
